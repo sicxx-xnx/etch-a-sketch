08f3b182400e7967300ceac42558fda7fd8a9546
sicxx-xnx <hpeyton33@gmail.com>
2025-07-25 13:48:23 -0500

    added popup and reset logix
    added the js logic to reset the etch a sketch container and give me a new one based on the entered inputs. also did the random color selector instead of having every hover color always be black

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const etchASketchContainer = document.querySelector("#container")
-let side = 100
+const etchASketchContainer = document.querySelector("#container");
+const resetButtoon = document.querySelector(".reset")
+const documentBody = document.querySelector("body")
+const popupLeftSideInput = document.querySelector("#leftsideInput")
+const popuptopSideInput = document.querySelector("#topsideInput")
+const popup = document.querySelector(".popup")
+const popupSubmit = document.querySelector('.popupSubmit')
+const popupHolder = document.querySelector(".PopupHolder")
 
-for (let i = 0; i < (16); i++) {
+resetButtoon.addEventListener("click", () => {popupHolder.style.display = "flex"})
+popupSubmit.addEventListener("click", popupvaule)
+popupHolder.addEventListener("click", () => popupHolder.style.display = "none")
+
+
+let randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+let side1 = 16;
+let side2 = 16
+
+
+function popupvaule() {
+
+side1 = popupLeftSideInput.value    
+side2 = popuptopSideInput.value
+etchASketchContainer.innerHTML = ""
+for (let i = 0; i < side1 ; i++) {
 const innerEtchASketchdiv = document.createElement("div")
 etchASketchContainer.appendChild(innerEtchASketchdiv)
 innerEtchASketchdiv.style.border = "2px solid black"
@@ -9,7 +30,7 @@ innerEtchASketchdiv.style.height =
 innerEtchASketchdiv.style.width = "100%"
 innerEtchASketchdiv.style.flex = `1 1 0`
 innerEtchASketchdiv.style.display = "flex" 
-for (let j = 0; j < 16; j++) {
+for (let j = 0; j < side2 ; j++) {
     const innerinnerdiv = document.createElement("div")
     innerEtchASketchdiv.appendChild(innerinnerdiv)
     innerinnerdiv.style.border = "2px solid black"
@@ -18,6 +39,38 @@ for (let j = 0; j < 16; j++) {
 }
 etchASketchContainer.addEventListener("mousemove", (e) => {if (e.target !== "innerinnerdiv") {
 e.target.style.backgroundColor = "black";
+etchASketchContainer.style.backgroundColor = "white"
+innerEtchASketchdiv.style.backgroundColor = "white"    
+} else { 
+e.target.style.backgroundColor = "white";
+etchASketchContainer.style.backgroundColor = "white"
+innerEtchASketchdiv.style.backgroundColor = "white"    
+} }
+
+)}
+
+popupHolder.style.display = "none"
+
+}
+
+for (let i = 0; i < side1 ; i++) {
+const innerEtchASketchdiv = document.createElement("div")
+etchASketchContainer.appendChild(innerEtchASketchdiv)
+innerEtchASketchdiv.style.border = "2px solid black"
+innerEtchASketchdiv.style.height = 
+innerEtchASketchdiv.style.width = "100%"
+innerEtchASketchdiv.style.flex = `1 1 0`
+innerEtchASketchdiv.style.display = "flex" 
+for (let j = 0; j < side2 ; j++) {
+    const innerinnerdiv = document.createElement("div")
+    innerEtchASketchdiv.appendChild(innerinnerdiv)
+    innerinnerdiv.style.border = "2px solid black"
+    innerinnerdiv.style.flex = "1"
+    innerinnerdiv.style.height = "100%"
+}
+etchASketchContainer.addEventListener("mousemove", (e) => {if (e.target !== "innerinnerdiv") {
+randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+e.target.style.backgroundColor = randomColor;
 etchASketchContainer.style.backgroundColor = "white"
 innerEtchASketchdiv.style.backgroundColor = "white"    
 } else { 
